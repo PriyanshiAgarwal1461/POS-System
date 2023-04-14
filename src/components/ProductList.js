c9a8a8308776f3c6812ea823d2552bda276c0ed3
PriyanshiAgarwal1461 <priyanshi0340@gmail.com>
2023-04-14 11:50:44 +0530

    add table of selected products on left side

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,24 +3,100 @@
 import React from 'react';
 import productsData from './data.json';
 import './Product.css'; // import CSS file
+import { useState } from 'react';
 
 export const ProductList = () => {
-  const { products } = productsData;
+  const [selectedProducts, setSelectedProducts] = useState([]);
+
+  const handleProductClick = (product) => {
+    const index = selectedProducts.findIndex((p) => p.name === product.name);
+    if (index !== -1) {
+      const updatedProducts = [...selectedProducts];
+      updatedProducts[index].quantity += 1;
+      setSelectedProducts(updatedProducts);
+    } else {
+      const newProduct = {
+        name: product.name,
+        price: product.price,
+        quantity: 1,
+      };
+      setSelectedProducts([...selectedProducts, newProduct]);
+    }
+  };
+
+  const handleQuantityChange = (event, index) => {
+    const updatedProducts = [...selectedProducts];
+    updatedProducts[index].quantity = parseInt(event.target.value);
+    setSelectedProducts(updatedProducts);
+  };
+
+  const tableRows = selectedProducts.map((product, index) => (
+    <tr key={index}>
+      <td>{product.name}</td>
+      <td>${product.price}</td>
+      <td>
+        <div className="quantity">
+          <button
+            onClick={() => {
+              const updatedProducts = [...selectedProducts];
+              updatedProducts[index].quantity -= 1;
+              setSelectedProducts(updatedProducts);
+            }}
+            disabled={product.quantity <= 1}
+          >
+            -
+          </button>
+          <input
+            type="number"
+            value={product.quantity}
+            onChange={(event) => handleQuantityChange(event, index)}
+          />
+          <button
+            onClick={() => {
+              const updatedProducts = [...selectedProducts];
+              updatedProducts[index].quantity += 1;
+              setSelectedProducts(updatedProducts);
+            }}
+          >
+            +
+          </button>
+        </div>
+      </td>
+      <td>${product.price * product.quantity}</td>
+    </tr>
+  ));
 
   return (
-    <div className="product-list-container" span={24}>
-      <div className="left-side" span={12}>
-        <p>THERE ARE NO PRODUCTS</p>
-
+    <div className="product-list-container">
+      <div className="left-side">      
+        {selectedProducts.length === 0 ? (
+          <p>There are no products</p>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total Price</th>
+              </tr>
+            </thead>
+            <tbody>{tableRows}</tbody>
+          </table>
+        )}
       </div>
-      <div className="right-side" span={12}>
-        {products.map((product) => (
-          <div key={product.name} className="product">
-            <img src={product.image} alt={product.name} className="product-image" />
+      <div className="right-side">
+        {productsData.products.map((product) => (
+          <div
+            key={product.id}
+            className="product"
+            onClick={() => handleProductClick(product)}
+          >
+            <img src={product.image} alt={product.name} className="product-image"/>
             <p>{product.name}</p>
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
